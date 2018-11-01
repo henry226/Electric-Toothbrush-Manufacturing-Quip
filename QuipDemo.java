e3a57b11d84ba20e831cc39a3a083dde22f5bec0
henry226 <henry5581984@gmail.com>
2018-11-01 14:37:10 -0400

    add employee, department and salary method

--- QuipDemo.java
+++ QuipDemo.java
@@ -8,32 +8,98 @@ public class QuipDemo {
 		
 		System.out.println("Hello and welcome to Quip!");
 		Scanner scan = new Scanner(System.in); // create scanner object
-		int option = 9; // initiate option with value 
 		
-		while (option != 0) {
+		// Ask for username and password
+		System.out.println("Enter Username:");
+		String username = scan.nextLine();
+		System.out.println("Enter Password:");
+		String password = scan.nextLine();
+		
+		String userType;
+		boolean validates;
+		
+		/*
+		 * In this test case, there are 2 users in the system.
+		 * Super user -> username: admin & password: 1234
+		 * Normal user -> username: user & password: 4321
+		 */
+		if(username.equals("admin") && password.equals("1234")) {
+			System.out.println("Welcome admin. You can view all datas.");
+			userType = "admin";
+			validates = true;
+		}
+		else if(username.equals("user") && password.equals("4321")) {
+			System.out.println("Welcome user. You can only view your data.");
+			userType = "user";
+			validates = true;
+		}
+		else {
+			System.out.println("Sorry, error username or password! Please try again."); 
+			userType = null;
+			validates = false;
+		}
+		
+		// initiate option with value 
+		int option = 9; 
+		while (option != 0 && validates == true) {
 			// show options to user, input 0 then exit
 			System.out.println("\nPlease Enter your option:\n1 -> HR System\n2 -> Account System\n0 -> Exit");
 			option = scan.nextInt(); // sacn the input
+			
 			if(option == 1) {
+				// user to choose options
+				System.out.println("Please enter your option:\na. View Employee Information\nb. View Department Information\nc. View Salary Intormation");
+				
+				Scanner scan2 = new Scanner(System.in);
+				String hrOption = scan2.nextLine();
+				
+				System.out.println("===============================");
 				HumanResourceFactory hrFactory = new HumanResourceFactory();
-				// String type, String firstName, String lastName, String socialSecurityNumber, int phoneNumber, 
-				// String title, int salary, String department
-				HumanResource employee1 = hrFactory.getInfo("EMPLOYEE", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
-						100000, "Computer Science");	// create employee object
-				employee1.showInfo();
+				if(hrOption.equals("a")) {
+					System.out.println("Employee Information");
+					Employee(hrFactory, userType);
+				}
+				else if(hrOption.equals("b")) {
+					System.out.println("Department Information");
+					Department(hrFactory, userType);
+				}
+				else if(hrOption.equals("c")){
+					System.out.println("Salary Information");
+					Salary(hrFactory, userType);
+				}
+				else 
+					System.out.println("Invalid input.");
 				
-				// create department objec
-				HumanResource employee1Dept = hrFactory.getInfo("DEPARTMENT", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
-						100000, "Computer Science"); 
-				employee1Dept.showInfo();
-				
-				// create salary object
-				HumanResource employee1Salary = hrFactory.getInfo("SALARY", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
-						100000, "Computer Science"); 
-				employee1Salary.showInfo();
-				
+				System.out.println("===============================");
+			}
+			else if(option == 0) {
+				System.out.println("Bye!");
 			}
 		}
 	}
-
+	
+	
+	// The employee method
+	public static void Employee(HumanResourceFactory hrFactory, String userType) {
+		// create employee object
+		HumanResource employee1 = hrFactory.getInfo("EMPLOYEE", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
+				100000, "Computer Science");	
+		employee1.showInfo();
+	}
+	
+	// The employee method
+	public static void Department(HumanResourceFactory hrFactory, String userType) {
+		// create department objec
+				HumanResource employee1Dept = hrFactory.getInfo("DEPARTMENT", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
+						100000, "Computer Science"); 
+				employee1Dept.showInfo();
+	}
+	
+	// The employee method
+	public static void Salary(HumanResourceFactory hrFactory, String userType) {
+		// create salary object
+		HumanResource employee1Salary = hrFactory.getInfo("SALARY", "MengTse", "Li", "111-11-1111", "617-617-6617", "Software Manager",
+				100000, "Computer Science"); 
+		employee1Salary.showInfo();
+	}
 }
